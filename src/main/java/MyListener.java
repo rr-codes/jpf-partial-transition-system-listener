126ed34202ac73e9b2d90b892aaa4707b1b7e20d
Richard Robinson <rir@my.yorku.ca>
2021-02-28 15:22:34 -0500

    updated gradle project; added listener skeleton

---
+++ src/main/java/MyListener.java
@@ -0,0 +1,60 @@
+package java;
+
+import gov.nasa.jpf.search.*;
+
+public class MyListener implements SearchListener {
+    @Override
+    public void stateAdvanced(Search search) {
+
+    }
+
+    @Override
+    public void stateProcessed(Search search) {
+
+    }
+
+    @Override
+    public void stateBacktracked(Search search) {
+
+    }
+
+    @Override
+    public void statePurged(Search search) {
+
+    }
+
+    @Override
+    public void stateStored(Search search) {
+
+    }
+
+    @Override
+    public void stateRestored(Search search) {
+
+    }
+
+    @Override
+    public void searchProbed(Search search) {
+
+    }
+
+    @Override
+    public void propertyViolated(Search search) {
+
+    }
+
+    @Override
+    public void searchStarted(Search search) {
+
+    }
+
+    @Override
+    public void searchConstraintHit(Search search) {
+
+    }
+
+    @Override
+    public void searchFinished(Search search) {
+
+    }
+}
