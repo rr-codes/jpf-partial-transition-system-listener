5e2543160e492b2e4b5178ad788b646f5ccb66a8
Richard Robinson <rir@my.yorku.ca>
2021-04-07 21:40:00 -0400

    added a lot of JavaDoc

--- src/main/partialtransitionsystemlistener/PartialTransitionSystemListener.java
+++ src/main/partialtransitionsystemlistener/PartialTransitionSystemListener.java
@@ -17,18 +17,37 @@ import java.util.Set;
 
 /**
  * Add a partial state space observer to JPF and build a graph of the state space
- * that is explored by JPF. The graph can be generated in different formats.
- * The current formats that are supported are DOT (visualized by a tool
- * like GraphViz from ATT - http://www.graphviz.org/) and TRA.
- * The graph is stored in a file called "jpf-state-space.<extension>" where
- * extension is ".dot" or ".tra". By default it generates a DOT graph.
+ * that is explored by JPF, as well as any unexplored states. The graph can be
+ * generated in different formats. The current formats that are supported are
+ * DOT and TRA. The graph is stored in a file called "jpf-state-space.extension"
+ * where extension is ".dot" or ".tra". By default it generates a DOT graph.
+ *
+ * <p></p>
+ *
+ * <b>Options</b> (all keys should be prefixed with {@code partialtransitionsystemlistener.}):
+ * <table>
+ * <tr>
+ * <td> <b>Key</b> </td> <td> <b>Type</b> </td> <td> <b>Default</b> </td>
+ * <td> <b>Description</b> </td>
+ * </tr>
+ * <tr>
+ * <td> {@code max_new_states} </td>
+ * <td> {@code Integer} </td> <td> {@code 0} </td> <td>The maximum amount of
+ * allowed states for this listener</td>
+ * </tr>
+ * <tr>
+ * <td> {@code use_dot} </td>
+ * <td> {@code Boolean} </td> <td> {@code true} </td> <td>If `true`, the
+ * generated file uses DOT notation; else, uses TRA notation.</td>
+ * </tr>
+ * </table>
  *
  * @see gov.nasa.jpf.JPFListener
  *
- * @author Richard Robinson [Implementation, Documentation]
- * @author Matt Walker [Implementation, Testing]
- * @author Allen Kaplan [Testing, Documentation]
- * @author Akin Adewale [Testing, Documentation]
+ * @author Richard Robinson [Implementation, Testing, Documentation]
+ * @author Matt Walker [Implementation, Testing, Documentation]
+ * @author Allen Kaplan [Documentation]
+ * @author Akin Adewale [Documentation]
  */
 @JPFOptions({
 		@JPFOption(type = "Int", key = "partialtransitionsystemlistener.max_new_states", defaultValue = "0",
@@ -51,6 +70,11 @@ public class PartialTransitionSystemListener extends SearchListenerAdapter {
 	private int newStates;
 	private int endState;
 
+	/**
+	 * Creates a new PartialTransitionSystemListener instance
+	 * @param config the config properties
+	 * @param jpf the JPF instance
+	 */
 	public PartialTransitionSystemListener(Config config, JPF jpf) {
 		this.transitions = new HashMap<>();
 
@@ -67,6 +91,16 @@ public class PartialTransitionSystemListener extends SearchListenerAdapter {
 		this.vm = jpf.getVM();
 	}
 
+	/**
+	 * Invoked when the search is started.
+	 *
+	 * @implNote Creates and instantiates a {@code PrintWriter} to be used for
+	 * output. The path of the outputted file is the SUT name of the VM
+	 * concatenated with either {@code .dot} or {@code .tra}, depending on the
+	 * configuration.
+	 *
+	 * @param search the Search instance
+	 */
 	public void searchStarted(Search search) {
 		String name = stateSpacePrinter.getFileName(search.getVM().getSUTName());
 		try {
@@ -77,6 +111,23 @@ public class PartialTransitionSystemListener extends SearchListenerAdapter {
 		}
 	}
 
+	/**
+	 * Invoked when a state has advanced.
+	 *
+	 * @implNote The output is not processed in this method. Instead, the
+	 * method adds the source and the target to a private MultiMap field
+	 * (a map whose keys are the source states and whose values are a list
+	 * of all the targets reached from the source). This is done to allow
+	 * for customization of the output formatting / processing by the
+	 * designated {@code PartialStateSpacePrinter}.
+	 *
+	 * <p></p>
+	 * In addition, this method does additional logic depending on if the
+	 * state is an end state or a new state, or if the number of new states
+	 * has been exceeded. In the latter case, the search terminates.
+	 *
+	 * @param search the Search instance
+	 */
 	@Override
 	public void stateAdvanced(Search search) {
 
@@ -103,6 +154,14 @@ public class PartialTransitionSystemListener extends SearchListenerAdapter {
 		}
 	}
 
+	/**
+	 * Invoked when the search has finished.
+	 *
+	 * @implNote This method delegates the output processing to the internal
+	 * {@code PartialStateSpacePrinter} field. After, the {@code writer} is closed.
+	 *
+	 * @param search the Search instance
+	 */
 	public void searchFinished(Search search) {
 		this.stateSpacePrinter.printResult(transitions, writer, endState);
 		this.writer.close();
